e76b7ca9fccd62b00c08e14b224f48107bc4b00e
Vanna <vanessa.km7@gmail.com>
2023-03-09 21:49:06 -0300

    add link navbar js

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,4 +1,30 @@
+const onLoadUserInfo = () => {
+    const email = localStorage.getItem('@WalletApp:userEmail');
+    const name = localStorage.getItem('@WalletApp:userName');
+    
+    const navbarUserInfo = document.getElementById("navbar-user-container");
+    const navbarUserAvatar = document.getElementById("navbar-user-avatar");
+
+    // Add user email
+    const emailElement = document.createElement("p");
+    const emailText = document.createTextNode(email);
+    emailElement.appendChild(emailText);
+    navbarUserInfo.appendChild(emailElement)
+
+    // Add logout link
+    const logoutElement = document.createElement("a");
+    const logoutText = document.createTextNode("Sair");
+    logoutElement.appendChild(logoutText);
+    navbarUserInfo.appendChild(logoutElement);
+
+    // Add user first letter inside avatar
+    const nameElement = document.createElement("h3");
+    const nameText = document.createTextNode(name.charAt(0));
+    nameElement.appendChild(nameText);
+    navbarUserAvatar.appendChild(nameElement);
+
+};
+
 window.onload = () => {
-    const email = localStorage.getItem("@WalletApp:userEmail");
-    console.log(email);
-}
+    onLoadUserInfo();
+};
